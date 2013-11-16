8f65dda619fb1667fd503ccbc1ffbc10b220a753
frostalf <frostalf@gmail.com>
2013-11-15 18:20:00 -0800

    Ignore casing for Text or Meta arguments since it gets made to Uppercase anyhow

--- src/main/java/io/snw/tutorial/TutorialCommands.java
+++ src/main/java/io/snw/tutorial/TutorialCommands.java
@@ -35,7 +35,7 @@ public class TutorialCommands implements CommandExecutor {
                 return true;
             }
             
-            if(!"META".equals(args[1]) || !"TEXT".equals(args[1])){
+            if(!"META".equalsIgnoreCase(args[1]) || !"TEXT".equalsIgnoreCase(args[1])){
                 sender.sendMessage(ChatColor.RED + "Must specify META or TEXT");
                 return true;
             }
