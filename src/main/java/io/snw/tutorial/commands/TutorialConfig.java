0a8839dcbc814c35f7ee733e723b9787e3c9c6b7
frostalf <frostalf@gmail.com>
2014-07-26 16:14:31 -0700

    Added command to edit config

--- src/main/java/io/snw/tutorial/commands/TutorialConfig.java
+++ src/main/java/io/snw/tutorial/commands/TutorialConfig.java
@@ -3,6 +3,7 @@ package io.snw.tutorial.commands;
 
 import io.snw.tutorial.ServerTutorial;
 import io.snw.tutorial.enums.Permissions;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -28,8 +29,13 @@ public class TutorialConfig implements CommandExecutor {
             if(Permissions.CONFIG.hasPerm(player)) {
                 plugin.getConfigConversation().editConfig(player);
                 return true;
+            } else {
+                sender.sendMessage(ChatColor.RED + "You don't have permission for this!");
+                return true;
             }
+        } else {
+            sender.sendMessage(ChatColor.RED + "This only accepts 1 input \"edit\"");
+            return true;
         }
-        return true;
     }
 }
